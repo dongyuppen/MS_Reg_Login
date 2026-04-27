e29de851db20c8528fc8fe333c7789f3957f13b5
dongyuppen <dongyuppen@naver.com>
2026-04-27 15:29:58 +0900

    first commit

---
+++ src/main/java/com/example/ms_reg_login/MsRegLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_reg_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRegLoginApplication.class, args);
+    }
+
+}
